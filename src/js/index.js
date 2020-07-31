51523c8600d40079942b0150c41a3460b81541ac
Ryszard Penkała <ryszard.penkala@gmail.com>
2020-07-31 15:35:00 +0200

    added hamburger list for mobiles

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 const UiSelectors = {
   dataHamburger: '[ data-hamburger ]',
+  dataLinks: '[ data-links ]',
 }
 
 const dataHamburger = document.querySelector(UiSelectors.dataHamburger);
+const dataLinks = document.querySelector(UiSelectors.dataLinks);
 
 dataHamburger.addEventListener('click', () => {
     dataHamburger.classList.toggle('hamburger--active');
+    dataLinks.classList.toggle('links--active');
     })
