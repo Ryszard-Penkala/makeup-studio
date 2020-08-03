232fe6e58be2158d1b85a361cd0e72b6c1241016
Ryszard Penkała <ryszard.penkala@gmail.com>
2020-08-03 07:49:30 +0200

    added hide hamburger menu functionality on click

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,21 @@
 const UiSelectors = {
   dataHamburger: '[ data-hamburger ]',
   dataLinks: '[ data-links ]',
+  dataLink: '[ data-link ]',
 };
 
 const dataHamburger = document.querySelector(UiSelectors.dataHamburger);
 const dataLinks = document.querySelector(UiSelectors.dataLinks);
+const dataLink = document.querySelectorAll(UiSelectors.dataLink);
 
 dataHamburger.addEventListener('click', () => {
   dataHamburger.classList.toggle('hamburger--active');
   dataLinks.classList.toggle('links--active');
+});
+
+dataLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    dataHamburger.classList.toggle('hamburger--active');
+    dataLinks.classList.toggle('links--active');
+  });
 });
