879cb1f3616b98d05607c7a652cdaeb34a3d8bec
Ryszard Penkała <ryszard.penkala@gmail.com>
2020-08-07 07:45:35 +0200

    added google maps

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,24 @@ const UiSelectors = {
   dataHamburger: '[ data-hamburger ]',
   dataLinks: '[ data-links ]',
   dataLink: '[ data-link ]',
+  dataMap: '[ data-map ]',
 };
 
 const dataHamburger = document.querySelector(UiSelectors.dataHamburger);
 const dataLinks = document.querySelector(UiSelectors.dataLinks);
 const dataLink = document.querySelectorAll(UiSelectors.dataLink);
+const dataMap = document.querySelector(UiSelectors.dataMap);
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(dataMap, {
+    center: { lat: 52.3915729, lng: 17.3839114 },
+    zoom: 10,
+  });
+}
+
+initMap();
 
 dataHamburger.addEventListener('click', () => {
   dataHamburger.classList.toggle('hamburger--active');
