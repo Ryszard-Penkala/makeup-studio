c6a7c73ee21b62e9100570592240385a2e7e674d
Ryszard Penkała <ryszard.penkala@gmail.com>
2020-08-13 07:49:00 +0200

    added initial slider

--- src/js/index.js
+++ src/js/index.js
@@ -34,3 +34,20 @@ dataLink.forEach((link) => {
 initMap();
 
 console.log(map);
+
+const swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
