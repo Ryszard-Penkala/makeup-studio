aa2bbe3bfb6cb1b67cf89f9d4c27ce48fc10721b
Ryszard Penkała <ryszard.penkala@gmail.com>
2020-08-18 07:06:27 +0200

    adjusting swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,24 @@
+// import Swiper from 'swiper/bundle';
+
 const UiSelectors = {
   dataHamburger: '[ data-hamburger ]',
   dataLinks: '[ data-links ]',
   dataLink: '[ data-link ]',
   dataMap: '[ data-map ]',
+  // dataSwiperContainer: '[ data-swiper_container ]',
+  // dataSwiperPagination: '[ data-swiper_pagination ]',
+  // dataSwiperNext: '[ data-button_next ]',
+  // dataSwiperPrev: '[ data-button_prev ]',
 };
 
 const dataHamburger = document.querySelector(UiSelectors.dataHamburger);
 const dataLinks = document.querySelector(UiSelectors.dataLinks);
 const dataLink = document.querySelectorAll(UiSelectors.dataLink);
 const dataMap = document.querySelector(UiSelectors.dataMap);
+// const dataSwiperContainer = document.querySelector(UiSelectors.dataSwiperContainer);
+// const dataSwiperPagination = document.querySelector(UiSelectors.dataSwiperPagination);
+// const dataSwiperNext = document.querySelector(UiSelectors.dataSwiperNext);
+// const dataSwiperPrev = document.querySelector(UiSelectors.dataSwiperPrev);
 
 let map;
 
@@ -33,21 +43,19 @@ dataLink.forEach((link) => {
 
 initMap();
 
-console.log(map);
-
-const swiper = new Swiper('.swiper-container', {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+// const swiper = new Swiper(dataSwiperContainer, {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: dataSwiperPagination,
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: dataSwiperNext,
+//     prevEl: dataSwiperPrev,
+//   },
+// });
